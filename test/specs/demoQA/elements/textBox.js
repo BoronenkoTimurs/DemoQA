a359b10c2a68a3337c8f43e3693cce51be7691c4
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2023-11-18 10:54:14 +0200

    Make test for Text Box

--- test/specs/demoQA/elements/textBox.js
+++ test/specs/demoQA/elements/textBox.js
@@ -1,8 +1,50 @@
-describe("Testing the box form", () => {
-  it("fill the form with valid date", async () => {
-    await browser.url("./");
+const assert = require("assert");
 
-    const TITLE = await browser.getTitle();
-    console.log(TITLE);
+const NAME = "p[id=name]";
+const EMAIL = "p[id=email]";
+const CURRENTADDRESS = "p[id=currentAddress]";
+const PERMANENTADDRESS = "p[id=permanentAddress]";
+
+const INPUTNAME = "input[type=text]";
+const INPUTEMAIL = "input[type=email]";
+const INPUTCURRENTADDRESS = "#currentAddress";
+const INPUTPERMANENTADDRESS = "#permanentAddress";
+const SUBMITBUTTON = "button[id=submit]";
+
+describe("Testing the box form", () => {
+  it("fill the form with valid data", async () => {
+    // Navigation
+    await browser.url("./");
+    await $("h5=Elements").click();
+    await $$("li")[0].click();
+
+    // Set data
+    await $(INPUTNAME).setValue("test123");
+    await $(INPUTEMAIL).setValue("test123@gmail.com");
+    await $(INPUTCURRENTADDRESS).setValue("Daugavpils");
+    await $(INPUTPERMANENTADDRESS).setValue("Riga");
+    await $(SUBMITBUTTON).click();
+
+    // Checking
+    await expect($(NAME)).toBeExisting();
+    await expect($(EMAIL)).toBeExisting();
+    await expect($(CURRENTADDRESS)).toBeExisting();
+    await expect($(PERMANENTADDRESS)).toBeExisting();
+  });
+  it("fill the form with not valid data", async () => {
+    // Navigation
+    await browser.url("./");
+    await $("h5=Elements").click();
+    await $("span=Text Box").click();
+
+    // Set values
+    await $(INPUTNAME).setValue("test123");
+    await $(INPUTEMAIL).setValue("invalidEmail");
+    await $(INPUTCURRENTADDRESS).setValue("Daugavpils");
+    await $(INPUTPERMANENTADDRESS).setValue("Riga");
+    await $(SUBMITBUTTON).click();
+
+    // Checking
+    await expect($(".field-error")).toBeExisting();
   });
 });
