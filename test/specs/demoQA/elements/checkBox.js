c17c66ce7d21e7cc425209b070268cbe0ac63537
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2023-11-19 09:26:01 +0200

    Check additional functional

--- test/specs/demoQA/elements/checkBox.js
+++ test/specs/demoQA/elements/checkBox.js
@@ -1,5 +1,16 @@
 describe("Testing of Check box example", () => {
   it("+, -, arrow left works properly", async () => {
+    // Functions
+    async function existingDestkop() {
+      const TEXT = "span=You have selected :";
+
+      $("span=Desktop").click();
+      expect($$(".rct-text")[0].$(".rct-icon-half-check")).toBeExisting();
+      expect($(TEXT).toHaveAttribute("span=desktop")).toBeExisting();
+      expect($(TEXT).toHaveAttribute("span=notes")).toBeExisting();
+      expect($(TEXT).toHaveAttribute("span=commands")).toBeExisting();
+    }
+
     // Navigation
     await browser.url("./");
     await $("h5=Elements").click();
@@ -9,7 +20,10 @@ describe("Testing of Check box example", () => {
     for (let i = 0; i < 5; i++) {
       await $$(".rct-icon-expand-close")[0].click();
     }
+    await $(".rct-icon-collapse-all").click();
+    await $(".rct-icon-expand-all").click();
 
     // Checking
+    await browser.call(existingDestkop);
   });
 });
